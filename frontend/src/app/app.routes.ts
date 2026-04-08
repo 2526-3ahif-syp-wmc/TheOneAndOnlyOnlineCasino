82b7e17c42ba314cbb451c2b79732098b0027696
osamahnbb <o.haj_najib@students.htl-leonding.ac.at>
2026-04-08 10:22:10 +0200

    added needed routes and components for my issues + home

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ContactPage } from './contact-page/contact-page';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path: 'home', component: Home },
+    {path: 'contact', component: ContactPage }
+];
---
+++ frontend/src/app/plinko-game/plinko-game.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-plinko-game',
+  imports: [],
+  templateUrl: './plinko-game.html',
+  styleUrl: './plinko-game.scss',
+})
+export class PlinkoGame {
+
+}
