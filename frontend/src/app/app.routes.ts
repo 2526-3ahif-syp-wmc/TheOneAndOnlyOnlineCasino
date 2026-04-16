4489987dc615aed23f9ee62971d607c3c46e625c
David <d.purcaru@students.htl-leonding.ac.at>
2026-04-16 22:11:17 +0200

    add sign up UI, no functions

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { ContactPage } from './contact-page/contact-page';
 import { Home } from './home/home';
+import { SignIn } from './sign-in/sign-in';
 
 
 export const routes: Routes = [
-    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path: '', redirectTo: '/sign-in', pathMatch: 'full'},
     {path: 'home', component: Home },
+    {path: 'sign-in', component: SignIn },
     {path: 'contact', component: ContactPage }
 ];
