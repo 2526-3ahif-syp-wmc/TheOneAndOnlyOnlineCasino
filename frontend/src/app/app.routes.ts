f1047f2025da83b57c271662e93c2402de351f56
osamahnbb <o.haj_najib@students.htl-leonding.ac.at>
2026-04-17 08:51:56 +0200

    added navbar and contact page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -8,5 +8,5 @@ export const routes: Routes = [
     {path: '', redirectTo: '/sign-in', pathMatch: 'full'},
     {path: 'home', component: Home },
     {path: 'sign-in', component: SignIn },
-    {path: 'contact', component: ContactPage }
+    {path: 'contact-page', component: ContactPage }
 ];
