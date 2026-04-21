fc23b378abe682779a909cfb1fea929768740a5f
David <d.purcaru@students.htl-leonding.ac.at>
2026-04-21 15:36:03 +0200

    fixed navbar (as component)

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,28 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { NavBar } from "./nav-bar/nav-bar";
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('frontend');
+  showNavbar = true;
+
+  constructor(private router: Router) {
+    this.updateNavbar(this.router.url);
+
+    this.router.events
+      .pipe(filter((event: any) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.updateNavbar(this.router.url);
+      });
+  }
+
+  private updateNavbar(url: string): void {
+    this.showNavbar = !url.includes('/auth');
+  }
 }
