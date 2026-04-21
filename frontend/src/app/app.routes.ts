e1bd09ba68362cb1db858bc7bbec418615541ebe
David <d.purcaru@students.htl-leonding.ac.at>
2026-04-21 15:27:37 +0200

    push

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -9,6 +9,6 @@ export const routes: Routes = [
     {path: '', redirectTo: '/auth', pathMatch: 'full'},
     {path: 'home', component: Home },
     {path: 'auth', component: Auth },
-    {path: 'contact', component: ContactPage },
+    {path: 'contact-page', component: ContactPage },
     {path: 'user-profile', component: UserProfile }
 ];
--- frontend/src/app/auth/auth.ts
+++ frontend/src/app/auth/auth.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './auth.html',
   styleUrl: './auth.scss',
 })
