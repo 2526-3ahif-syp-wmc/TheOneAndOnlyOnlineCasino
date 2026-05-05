0965aae596822ed263552c7c88e747a62117503a
David <d.purcaru@students.htl-leonding.ac.at>
2026-05-05 23:18:42 +0200

    added better games.html and scss, no functionalities yet!

--- frontend/src/app/home/home.ts
+++ frontend/src/app/home/home.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { UserService } from '../services/user-service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
