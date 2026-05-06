1c0c7f0033bfbc35e1020eff4f621bd40e837360
m61-kanmaz <m.kanmaz@students.htl-leonding.ac.at>
2026-05-06 20:32:09 +0200

    creating first game component

---
+++ frontend/src/app/roulette-game/roulette-game.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-roulette-game',
+  imports: [],
+  templateUrl: './roulette-game.html',
+  styleUrl: './roulette-game.scss',
+})
+export class RouletteGame {}
