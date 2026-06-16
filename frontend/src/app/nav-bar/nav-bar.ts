27e613dec083542c95de4e3b73374adc806b96b5
osamahnbb <o.haj_najib@students.htl-leonding.ac.at>
2026-06-16 10:44:44 +0200

    made profile pic visible in navbar icon

--- frontend/src/app/nav-bar/nav-bar.ts
+++ frontend/src/app/nav-bar/nav-bar.ts
@@ -1,21 +1,22 @@
 import { Component, computed, inject } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { UserService } from '../services/user-service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './nav-bar.html',
   styleUrls: ['./nav-bar.scss'],
 })
 export class NavBar {
   private service = inject(UserService);
-
   private router = inject(Router);
 
   protected username = this.service.username;
   protected coins = this.service.coins;
   protected premium = this.service.premium;
+  protected avatarUrl = this.service.avatarUrl;
 
   protected isPremium = computed(() => Number(this.premium()) === 1);
   protected avatarLetter = computed(() => this.service.username().charAt(0).toUpperCase());
